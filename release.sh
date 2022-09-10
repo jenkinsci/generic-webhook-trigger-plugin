483e84bcd1950591a394d482d3a027dbe89b1e80
Tomas Bjerre <tomas.bjerre85@gmail.com>
2022-09-10 08:14:41 +0200

    chore: setting version 0.1.0-SNAPSHOT

--- release.sh
+++ release.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-./mvnw release:prepare release:perform -B || exit 1
-./mvnw package
-git commit -a --amend --no-edit
-git push -f
+
+gpg -o /tmp/dummy --sign .gitignore \
+ && nextVersion=$(npx git-changelog-command-line --print-next-version) \
+ && ./mvnw versions:set -DnewVersion=$nextVersion-SNAPSHOT \
+ && git commit -a -m "chore: setting version $nextVersion-SNAPSHOT"
+ && ./mvnw release:prepare release:perform -B -DperformRelease=true \
+ && npx git-changelog-command-line -of CHANGELOG.md -ip ".*maven-release-plugin.*" \
+ && git commit -a -m "chore: updating changelog" \
+ && git push \
+ || git clean -f && git checkout pom.xml
