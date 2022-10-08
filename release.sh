ebfaa8f58c0949ec9336ea13b6425354bfde34fd
Tomas Bjerre <tomas.bjerre85@gmail.com>
2022-10-08 07:57:31 +0200

    fix: release-script

--- release.sh
+++ release.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 gpg -o /tmp/dummy --sign .gitignore \
+ && nextVersion=$(npx git-changelog-command-line --print-next-version) \
+ && ./mvnw versions:set -DnewVersion=$nextVersion-SNAPSHOT \
+ && git commit -a -m "chore: setting version $nextVersion-SNAPSHOT"
  
 ./mvnw release:prepare release:perform -B -DperformRelease=true \
  && npx git-changelog-command-line -of CHANGELOG.md -ip ".*maven-release-plugin.*" \
