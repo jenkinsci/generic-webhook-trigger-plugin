87206dcd28ed57b8a453a1965822e8e30b24df5a
Tomas Bjerre <tomas.bjerre85@gmail.com>
2023-11-27 19:45:08 +0100

    fix: contributing variables was broken (refs #285)

--- src/main/java/org/jenkinsci/plugins/gwt/GenericWebhookEnvironmentContributor.java
+++ src/main/java/org/jenkinsci/plugins/gwt/GenericWebhookEnvironmentContributor.java
@@ -1,18 +1,19 @@
 package org.jenkinsci.plugins.gwt;
 
+import com.google.common.base.Charsets;
 import hudson.EnvVars;
 import hudson.Extension;
 import hudson.model.EnvironmentContributor;
 import hudson.model.Run;
 import hudson.model.TaskListener;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Map;
 import javax.annotation.Nonnull;
 
 @Extension
 public class GenericWebhookEnvironmentContributor extends EnvironmentContributor {
-  private static final String CONTRIBUTING_VARIABLES =
-      GenericWebhookEnvironmentContributor.class.getSimpleName();
 
   @SuppressWarnings("unchecked")
   @Override
@@ -24,28 +25,26 @@ public class GenericWebhookEnvironmentContributor extends EnvironmentContributor
     final GenericCause cause = (GenericCause) r.getCause(GenericCause.class);
     if (cause != null) {
       final boolean shouldLog =
-          (cause.isPrintContributedVariables() || cause.isPrintPostContent())
-              && !cause.isPostContentPrinted();
+          (cause.isPrintContributedVariables() || cause.isPrintPostContent()) && !cause.isPostContentPrinted();
       if (shouldLog) {
         cause.setPostContentPrinted(true);
-        if (cause.isPrintPostContent()) {
-          listener.getLogger().println(CONTRIBUTING_VARIABLES);
-          listener.getLogger().println(" Received:\n\n" + cause.getPostContent() + "\n\n");
+      }
+      if (shouldLog && cause.isPrintPostContent()) {
+        listener.getLogger().println(" Received:\n\n" + cause.getPostContent() + "\n\n");
+      }
+      final Map<String, String> resolvedVariables = cause.getResolvedVariables();
+      if (shouldLog && cause.isPrintContributedVariables()) {
+        listener.getLogger().println("Contributing variables:\n");
+      }
+      for (final String variable : resolvedVariables.keySet()) {
+        final String resolved = cause.getResolvedVariables().get(variable);
+        if (shouldLog && cause.isPrintContributedVariables()) {
+          listener.getLogger().println("    " + variable + " = " + resolved);
         }
-        final Map<String, String> resolvedVariables = cause.getResolvedVariables();
-        if (cause.isPrintContributedVariables()) {
-          listener.getLogger().println("Contributing variables:\n");
-        }
-        for (final String variable : resolvedVariables.keySet()) {
-          final String resolved = cause.getResolvedVariables().get(variable);
-          if (cause.isPrintContributedVariables()) {
-            listener.getLogger().println("    " + variable + " = " + resolved);
-          }
-          envs.override(variable, resolved);
-        }
-        if (cause.isPrintContributedVariables()) {
-          listener.getLogger().println("\n");
-        }
+        envs.override(variable, resolved);
+      }
+      if (shouldLog && cause.isPrintContributedVariables()) {
+        listener.getLogger().println("\n");
       }
     }
   }
